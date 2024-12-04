1ce5be0469d661c61029a987014cba811fae7f2d
EirronD <dagleeirron27@gmail.com>
2024-12-04 23:53:36 +0800

    try

--- src/components/Admin/Notification.jsx
+++ src/components/Admin/Notification.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react'
 import BgImage from '../../assets/BgImage.jpg'
 import { db } from '../../Firebase/firebaseConfig.js'
